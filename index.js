d8da90bfde0d935a48223afa4aa9cd381aad0666
Simon Sun <simonthesun@yahoo.com>
2018-09-18 08:16:43 -0400

    lab1

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const API_KEY = "API_KEY_HERE";
+const API_KEY = "6f433300-bafc-11e8-88c5-811c39b2c016";
+let hashid = location.hash.substring(1);
 
 document.addEventListener("DOMContentLoaded", () => {
   const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
-  showGalleries(url);
+  if (hashid == "") { 
+    showGalleries(url);
+  } else {
+    showObjectsTable(hashid);
+  }
+
+  document.getElementById("bbutton").onclick = function () {
+      location.href = "index.html";
+  };
+  
 });
 
 function showGalleries(url) {
@@ -11,11 +21,11 @@ function showGalleries(url) {
   .then(data => {
     data.records.forEach(gallery => {
       document.querySelector("#galleries").innerHTML += `
-        <li>
+        <td>
           <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
             Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
           </a>
-        </li>
+        </td>
       `;
     });
     if (data.info.next) {
@@ -27,4 +37,85 @@ function showGalleries(url) {
 function showObjectsTable(id) {
   document.querySelector("#all-objects").style.display = "block";
   document.querySelector("#all-galleries").style.display = "none";
+  document.querySelector("#objectpage").style.display = "none";
+
+  objsUrl = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&size=99&gallery=${id}`;
+  document.querySelector("#objects").innerHTML = `
+    <tr>
+      <th>Title</th>
+      <th>Image</th>
+      <th>People</th>
+      <th>URL</th>
+    </tr>
+  `;
+
+  fetch(objsUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.records.forEach(object => {
+      ppl = "None Listed";
+      if (object.people) {
+        for (var i = 0; i < object.people.length; i++) {
+          if (i > 0) {
+            ppl = ppl + ", " + object.people[i].name;
+          } else {
+            ppl = object.people[i].name;
+          }
+        }
+      }
+
+      document.querySelector("#objects").innerHTML += `
+        <tr>
+          <td><a onclick="showObjectInfo('${object.objectnumber}', ${id})">${object.title}</td>
+          <td><img src='${object.primaryimageurl}'></td>
+          <td>${ppl}</td>
+          <td><a href='${object.url}'>See More</a></td>
+        </tr>
+      `;
+    });
+    if (data.info.next) {
+      showGalleries(data.info.next);
+    }
+  })
+}
+
+function showObjectInfo(objNum, galleryId) {
+  document.querySelector("#all-objects").style.display = "none";
+  document.querySelector("#all-galleries").style.display = "none";
+  document.querySelector("#objectpage").style.display = "block";
+
+  objUrl = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&objectnumber=${objNum}`;
+  document.querySelector("#objectpage").innerHTML = ``;
+  
+  fetch(objUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.records.forEach(object => {
+      
+      document.querySelector("#objectpage").innerHTML += `
+        <h2>${object.title}</h2>
+        <button onclick='showObjectsTable(${galleryId})'>Back</button>
+        <br>
+        <img src='${object.primaryimageurl}'>
+        <div id="single-object">
+            
+        </div>
+      `;
+
+      document.querySelector("#single-object").innerHTML += `
+        <p>
+          Description: ${object.description}
+        </p>
+        <p>
+          Provenance: ${object.provenance}
+        </p>
+        <p>
+          Accession Year: ${object.accessionyear}
+        </p>
+      `;
+    });
+    if (data.info.next) {
+      showGalleries(data.info.next);
+    }
+  })
 }
